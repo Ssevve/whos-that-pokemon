bfa849a770ad61d408bbd9f4b589e6653ff24784
Seweryn <ssevve@gmail.com>
2022-07-24 15:35:48 +0200

    Clean up js code

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ const pokemonImage = document.querySelector('img');
 const pElement = document.querySelector('p');
 const guessInput = document.querySelector('#guess');
 
+document.querySelector('form').addEventListener('submit', checkGuess);
+
+
 let score = 0;
 let highScore = localStorage.getItem('highScore') || 0;
 updateScoreUi();
@@ -11,13 +14,11 @@ let userGuessed = false;
 getRandomPokemonById();
 
 document.addEventListener('keydown', (e) => {
-  // Allow the user to skip to the next pokemon with enter key if they have guessed already
+  // Allow the user to skip to the next pokemon with the ENTER key if they have guessed already
   if (userGuessed && e.key === 'Enter') {
     getRandomPokemonById();
   }
 });
-
-document.querySelector('form').addEventListener('submit', checkGuess);
 
 const buttons = [...document.querySelectorAll('button')];
 buttons.forEach(button => {
@@ -36,6 +37,33 @@ buttons.forEach(button => {
     }
   });
 });
+
+async function getRandomPokemonById() {
+  const id = generateRandomId();
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+      
+    // Reset game state
+    pokemonName = '';
+    userGuessed = false;
+    guessInput.value = '';
+    guessInput.classList.remove('wrong');
+    guessInput.classList.remove('correct');
+    pElement.classList.add('hidden');
+
+    updateScoreUi();
+    
+    pokemonImage.src = data.sprites.front_default;
+    pokemonImage.addEventListener('load', () => pokemonImage.classList.remove('fade-in-animation'));
+    pokemonName = data.name;     
+
+  } catch (err) {
+    console.log(`error: ${err}`);
+  }    
+}
 
 
 function checkGuess(e) {
@@ -67,37 +95,11 @@ function checkGuess(e) {
   }
 }
 
-async function getRandomPokemonById() {
-  const id = generateRandomId();
-  const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
-
-  try {
-    const res = await fetch(url);
-    const data = await res.json();
-      
-    // Reset game state
-    pokemonName = '';
-    userGuessed = false;
-    guessInput.value = '';
-    guessInput.classList.remove('wrong');
-    guessInput.classList.remove('correct');
-    pElement.classList.add('hidden');
-
-    updateScoreUi();
-    
-    pokemonImage.src = data.sprites.front_default;
-    pokemonImage.addEventListener('load', () => pokemonImage.classList.remove('fade-in-animation'));
-    pokemonName = data.name;     
-
-  } catch (err) {
-    console.log(`error: ${err}`);
-  }
-        
-}
 
 function generateRandomId() {
   return Math.floor(Math.random() * 151 + 1); // only the 1st gen (151 pokemon)
 }
+
 
 function updateScoreUi() {
   document.querySelector('.score').textContent = score;
